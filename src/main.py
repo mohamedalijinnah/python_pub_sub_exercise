0289b30822e15b60ce8dc00c0eab80d82d9bd9cd
Mohamed Ali Jinnah <mailtojinnah@gmail.com>
2025-02-01 18:00:07 +0100

    Retrieving NL latitude and longitude boundary to simulate geo locations

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 # Sample program to demonstrate multiple publishers
 
-
 def publish_geo_data():
     print("Publishing NL geo data")
     # Reference https://www.latlong.net/
     return (52.132633, 5.291266)
+
+def get_geo_data_boundary():
+    print("NL Latitude and Longitude boundary")
+    # Reference https://latitudelongitude.org/nl/
+    return (50.77083, 53.35917), (3.57361, 7.10833)
 
 if __name__ == '__main__':
    print("Simulate a geo data publishers")
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,12 +2,15 @@ import sys
 import os 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 import unittest
-from main import publish_geo_data
+from main import publish_geo_data, get_geo_data_boundary
 
 class TestMain(unittest.TestCase):
 
     def test_main(self):
         self.assertEqual(publish_geo_data(), (52.132633, 5.291266))
+    
+    def test_nl_geo_data_boundary(self):
+        self.assertEqual(get_geo_data_boundary(), ((50.77083, 53.35917), (3.57361, 7.10833)))
 
 if __name__ == '__main__':
     unittest.main()
