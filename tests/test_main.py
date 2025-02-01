afdae8bd0569b7de1c50afd57fde8b31fdad785e
Mohamed Ali Jinnah <mailtojinnah@gmail.com>
2025-02-01 18:07:10 +0100

    Improved coverage

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,5 +13,11 @@ class TestMain(unittest.TestCase):
     def test_nl_geo_data_boundary(self):
         self.assertEqual(NL_LAT_LONG_BOUNDARY, ((50.77083, 53.35917), (3.57361, 7.10833)))
 
+    def test_nl_geo_data_boundary_latitude(self):
+        self.assertEqual(NL_LAT_LONG_BOUNDARY[0], ((50.77083, 53.35917)))
+
+    def test_nl_geo_data_boundary_longitude(self):
+        self.assertEqual(NL_LAT_LONG_BOUNDARY[1], ((3.57361, 7.10833)))       
+
 if __name__ == '__main__':
     unittest.main()
