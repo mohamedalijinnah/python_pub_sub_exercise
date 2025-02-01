2ebc73f425da02cc145699b1689b35df8931c07d
Mohamed Ali Jinnah <mailtojinnah@gmail.com>
2025-02-01 19:10:39 +0100

    included seconds randomizer and added test case

--- src/const.py
+++ src/const.py
@@ -1,2 +1,3 @@
 # Reference https://latitudelongitude.org/nl/
 NL_LAT_LONG_BOUNDARY = (50.77083, 53.35917), (3.57361, 7.10833)
+SECONDS_RANGE = (1.01, 5)
---
+++ src/time_randomizer.py
@@ -0,0 +1,4 @@
+from random import uniform
+
+def generate_random_seconds(min, max):
+    return uniform(min, max)
